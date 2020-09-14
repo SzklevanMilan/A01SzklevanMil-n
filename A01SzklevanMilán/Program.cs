6e615268fde89ab01df1d8a677654e3aceb77fd7
SzklevanMilan <szklevanm@gmail.com>
2020-09-14 10:53:19 +0200

    Add project files.

---
+++ A01SzklevanMilán/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace A01SzklevanMilán
+{
+    class Program
+    {
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("adatok.dat");
+        }
+        static void Main(string[] args)
+        {
+            Beolvasas();
+        }
+    }
+}
